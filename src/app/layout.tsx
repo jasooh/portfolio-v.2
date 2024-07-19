6d4663e3b382478f0c4f119c2ec76ef35d3bffb0
jasooh <abuyuanjustin@gmail.com>
2024-07-18 22:26:36 -0400

    Created name section & content section

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
       <body className={montserrat.className}>
         <ThemeProvider
           attribute="class"
-          defaultTheme="dark"
+          defaultTheme="system"
           enableSystem
           disableTransitionOnChange
         >
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,39 @@
+// CSS
 import "../styling/constants.css";
 
 export default function Home() {
   return (
-    <main className="page-container">
-      <div className="col-container">
-        <h1>Hello World!</h1>
-      </div>
+    <main className="page-container px-96 pt-20">
+      {/* Name section */}
+      <header className="fixed fit-self">
+        <p>Hey, I'm</p>
+        <h1 className="text-stroke mb-2">
+          JUSTIN <br /> ABUYUAN
+        </h1>
+        <h2>
+          Engineering Student @ the{" "}
+          <span className="text-secondary">University of Waterloo</span>
+        </h2>
+      </header>
+
+      {/* Content section */}
+      <section className="full-screen-fit row-container align-right">
+        {/* Summary */}
+        <div className="w-1/2">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum,
+            dicta assumenda pariatur minima nisi est fuga veniam aliquid
+            architecto eos eum nobis ut adipisci, vero, consectetur voluptas
+            ullam dolorem explicabo!
+          </p>
+        </div>
+
+        {/* Experience */}
+
+        {/* Projects */}
+
+        {/* Contact */}
+      </section>
     </main>
   );
 }
