02cba0fc4a4d2d319f0fe146802db0b824a81597
jasooh <abuyuanjustin@gmail.com>
2024-07-21 21:58:54 -0400

    Card component fix

--- src/components/ui/Card.tsx
+++ src/components/ui/Card.tsx
@@ -10,7 +10,7 @@ const Card = React.forwardRef<
   <div
     ref={ref}
     className={cn(
-      "rounded-lg border border-transparent text-foreground shadow-sm hover:border-secondary hover:bg-primary-foreground duration-300",
+      "h-full flex justify-between col-container rounded-lg border border-transparent text-foreground shadow-sm hover:border-secondary hover:bg-primary-foreground duration-300",
       className
     )}
     {...props}
@@ -24,7 +24,7 @@ const CardHeader = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <div
     ref={ref}
-    className={cn("flex flex-col space-y-1.5 p-6", className)}
+    className={cn("col-container space-y-1.5 p-6", className)}
     {...props}
   />
 ));
