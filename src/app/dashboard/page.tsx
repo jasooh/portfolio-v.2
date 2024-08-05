72980dcfd40fdfcad9e1db2b65b5debcf66105b4
jasooh <abuyuanjustin@gmail.com>
2024-08-04 21:31:52 -0400

    Added custom scroll bar

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,18 @@
+// CSS
+import "@/styling/constants.css";
+
+// Page component
+import PortfolioPage from "@/components/sections/portfolio/PortfolioPage";
+
+// Background effect
+import BackgroundEffect from "@/components/three/BackgroundEffect";
+
+export default function Home() {
+  return (
+    <main className="page-container col-container items-center justify-center">
+      <header>
+        <h2>Admin Panel</h2>
+      </header>
+    </main>
+  );
+}
