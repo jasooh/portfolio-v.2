b00f1c99822ee66243248c9d7f2204f8fe7de762
Justin Abuyuan <abuyuanjustin@gmail.com>
2024-10-11 15:02:20 -0400

    making sphere more visible

--- src/components/three/BackgroundEffect.tsx
+++ src/components/three/BackgroundEffect.tsx
@@ -8,7 +8,7 @@ import { Canvas } from "@react-three/fiber";
 
 export default function BackgroundEffect() {
   return (
-    <div className="fixed w-screen h-screen flex items-center justify-center opacity-25 pointer-events-none">
+    <div className="fixed w-screen h-screen flex items-center justify-center pointer-events-none">
       <Canvas>
         <SphereEffect />
       </Canvas>
